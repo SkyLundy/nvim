aab245d2ca0c0819d7225e6c83af7f9e0ca5f3ff
Sky Lundy <hello@skylundy.com>
2025-09-29 06:22:37 -0700

    Nonsense

--- init.lua
+++ init.lua
@@ -1,2 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Transparent background
+-- vim.cmd([[
+--   highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+-- ]])
+
+require("aerial").setup({
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
+})
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,8 @@
 return {
   --{ "maxmx03/fluoromachine.nvim" },
+
+  { "2giosangmitom/nightfall.nvim" },
+
   -- {
   --   "2giosangmitom/nightfall.nvim",
   --   lazy = false,
@@ -11,9 +14,9 @@ return {
   --   end,
   -- },
 
-  -- { "niyabits/calvera-dark.nvim" },
+  { "niyabits/calvera-dark.nvim" },
 
-  -- { "Everblush/nvim", name = "everblush" },
+  { "Everblush/nvim", name = "everblush" },
 
   -- {
   --   "neko-night/nvim",
@@ -27,11 +30,90 @@ return {
   -- },
 
   { "JoosepAlviste/palenightfall.nvim" },
+  { "w0ng/vim-hybrid" },
+  { "vim-scripts/Tango2" },
+  { "optixx/vim-optixx" },
+  { "ryross/ryderbeans" },
+  { "rebelot/kanagawa.nvim" },
+  { "savq/melange-nvim" },
+
+  { "avuenja/shizukana.nvim" },
+
+  {
+    "cdmill/neomodern.nvim",
+    config = function()
+      require("neomodern").setup({})
+    end,
+  },
+
+  { "sainnhe/edge" },
+
+  { "r1cardohj/zzz.vim" },
+
+  {
+    "febyeji/bluehour.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.termguicolors = true
+      -- vim.cmd.colorscheme("bluehour")
+    end,
+  },
+
+  {
+    "ricardoraposo/nightwolf.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("nightwolf").setup({
+        theme = "gray", -- 'black', 'dark-blue', 'gray', 'dark-gray', 'light'
+        italic = true,
+        transparency = false,
+        palette_overrides = {},
+        highlight_overrides = {},
+      })
+
+      -- vim.cmd.colorscheme("nightwolf")
+    end,
+  },
+
+  {
+    "romanaverin/charleston.nvim",
+    name = "charleston",
+    priority = 1000,
+  },
+
+  {
+    "clearaspect/onehalf",
+    lazy = false,
+    priority = 1000,
+  },
+
+  { -- Good
+    "Vallen217/eidolon.nvim", -- 'eidolon-twilight', eidolon-dusk', 'eidolon-midnight'
+    lazy = false,
+    priority = 1000,
+    -- config = function()
+    --     vim.cmd [[colorscheme eidolon]]
+    -- end
+  },
+
+  {
+    "Zeioth/neon.nvim",
+    opts = {
+      dim_inactive = false,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+      },
+    },
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "palenightfall",
+      colorscheme = "eidolon-dusk",
     },
   },
 }

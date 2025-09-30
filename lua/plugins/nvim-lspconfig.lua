c1b6b3d110d10a70e7f1320a149c31eeef14f708
Sky Lundy <hello@skylundy.com>
2025-09-30 11:09:56 -0700

    Add emmet, configure for PHP

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,44 @@
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      emmet_language_server = {
+        filetypes = {
+          "html",
+          "php",
+          "typescriptreact",
+          "javascriptreact",
+          "css",
+          "sass",
+          "scss",
+          "less",
+          "javascript",
+          "typescript",
+          "markdown",
+        },
+        init_options = {
+          ---@type table<string, string>
+          includeLanguages = {},
+          --- @type string[]
+          excludeLanguages = {},
+          --- @type string[]
+          extensionsPath = {},
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+          preferences = {},
+          --- @type boolean Defaults to `true`
+          showAbbreviationSuggestions = true,
+          --- @type "always" | "never" Defaults to `"always"`
+          showExpandedAbbreviation = "always",
+          --- @type boolean Defaults to `false`
+          showSuggestionsAsSnippets = false,
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+          syntaxProfiles = {},
+          --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+          variables = {},
+        },
+      },
+    },
+  },
+}
